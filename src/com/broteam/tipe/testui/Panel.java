b9b38113b2bac8563d3ce6b12b0f69eb55705380
Titouan BION <titouan.bion@gmail.com>
2014-01-14 23:31:33 +0100

    Ajout de forme rectangulaire pour tester

--- src/com/broteam/tipe/testui/Panel.java
+++ src/com/broteam/tipe/testui/Panel.java
@@ -7,6 +7,8 @@ public class Panel extends JPanel {
   @Override
 public void paintComponent(Graphics g){
     //x1, y1, width, height
-    g.drawRect(20, 20, 100, 200);
+    g.drawRect(50, 50, 100, 100);
+    g.drawRect(50, 50, 100, 50);
+    g.drawRect(50, 50, 50, 50);
   }
 }
