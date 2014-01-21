9a536ff719e8fed1ba095b44216755ce46bfe80f
Titouan BION <titouan.bion@gmail.com>
2014-01-21 23:56:59 +0100

    Correction bug room pleine mal formée.

--- src/com/broteam/tipe/shape/Room.java
+++ src/com/broteam/tipe/shape/Room.java
@@ -8,15 +8,30 @@ public class Room extends Shape {
 	Room(Point ptPress, Point ptRel, Color c) {
 		refPressed = ptPress;
 		refReleased = ptRel;
+		int px = refPressed.getX();
+		int py = refPressed.getY();
+		int rx = refReleased.getX();
+		int ry = refReleased.getY();
+		if ((px > rx) && (py > ry)) {
+				refPressed = new Point(rx, ry);
+				refReleased = new Point(px, py);
+			} else if (px > rx) {
+				refPressed = new Point(rx, py);
+				refReleased = new Point(px, ry);
+			} else if (py > ry) {
+				refPressed = new Point(rx, ry);
+				refReleased = new Point(px, py);
+			}
 		col = c;
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(col);
-		// TODO Faire un if pour les rectangles tracé à l'envers c'est à dire
-		// pressed à droite du released.
-		g.drawRect(refPressed.getX(), refPressed.getY(), refReleased.getX()
-				- refPressed.getX(), refReleased.getY() - refPressed.getY());
+		int px = refPressed.getX();
+		int py = refPressed.getY();
+		int rx = refReleased.getX();
+		int ry = refReleased.getY();
+		g.drawRect(px, py, Math.abs(rx - px), Math.abs(ry - py));
 	}
 }
