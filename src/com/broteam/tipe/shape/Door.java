238f769dbb4ef54c0b109871ad6b8091538f8b28
Titouan BION <titouan.bion@gmail.com>
2014-04-15 10:00:52 +0200

    Correction de bug majeur
    Mise en commentaire du code non fonctionnel dans Door.java pour
    compilation.

--- src/com/broteam/tipe/shape/Door.java
+++ src/com/broteam/tipe/shape/Door.java
@@ -2,8 +2,8 @@ package com.broteam.tipe.shape;
 
 public class Door {
     Door(Point ptPress, Point ptRel){
-        ptRel^2 + ptPress2
-        if (ptRel < ) {
+       // ptRel^2 + ptPress2
+       // if (ptRel < ) {
 
         }
     }
