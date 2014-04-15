1f59f7d4e54ff54f7261d3217bd9055dcf9c0ee0
Titouan BION <titouan.bion@gmail.com>
2014-04-15 15:28:08 +0200

    Pop-up d'information noShapeChosen

--- src/com/broteam/tipe/shape/NoShapeChosenException.java
+++ src/com/broteam/tipe/shape/NoShapeChosenException.java
@@ -1,18 +1,23 @@
 package com.broteam.tipe.shape;
 
+import javax.swing.JOptionPane;
+
 /**
  * Created by Titouan on 18/02/14.
  */
 public class NoShapeChosenException extends RuntimeException {
     public NoShapeChosenException() {
         super("Aucune forme sélectionnée");
+        JOptionPane.showMessageDialog(null, "Aucune forme sélectionnée", "Attention !", javax.swing.JOptionPane.WARNING_MESSAGE);
     }
 
     public NoShapeChosenException(String message) {
         super(message);
+        JOptionPane.showMessageDialog(null, message, "Attention !", javax.swing.JOptionPane.WARNING_MESSAGE);
     }
 
     public NoShapeChosenException(String message, Throwable cause) {
         super(message, cause);
+        JOptionPane.showMessageDialog(null, message+" Cause : "+cause, "Attention !", javax.swing.JOptionPane.WARNING_MESSAGE);
     }
 }
