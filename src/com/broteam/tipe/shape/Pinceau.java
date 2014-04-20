fa92de524da3c3f9cab439295b6be803b7e65529
Joffrey BION <joffrey.bion@gmail.com>
2014-04-20 19:35:08 +0200

    refactoring to use Java 2D

--- src/com/broteam/tipe/shape/Pinceau.java
+++ src/com/broteam/tipe/shape/Pinceau.java
@@ -1,56 +1,75 @@
 package com.broteam.tipe.shape;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Shape;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 
 import com.broteam.tipe.testui.Panel;
 
 public class Pinceau extends MouseAdapter {
 
-    private Point ptPress;
+    private Point2D ptPress;
     private static int shapeSelector = 0;
 
-	public static void setRoom() {
-		shapeSelector = 1;
-	}
+    public static void setRoom() {
+        shapeSelector = 1;
+    }
 
-	public static void setWall() {
-		shapeSelector = 2;
-	}
+    public static void setWall() {
+        shapeSelector = 2;
+    }
 
-	public static void setDoor() {
-		shapeSelector = 3;
-	}
+    public static void setDoor() {
+        shapeSelector = 3;
+    }
 
-	void setColor(Color c) {
-	}
+    void setColor(Color c) {
+    }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		ptPress = new Point(e.getX(), e.getY());
-        Shape toDraw = getNewShape(ptPress, ptPress);
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getButton() != MouseEvent.BUTTON1) {
+            System.out.println(e.getButton());
+            return;
+        }
+        ptPress = e.getPoint();
         Panel screen = (Panel) e.getSource();
-        screen.add(toDraw);
-	}
+        try {
+            screen.add(getNewShape(ptPress, ptPress));
+        } catch (NoShapeChosenException e1) {
+            ptPress = null;
+        }
+    }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        Point ptDrag = new Point(e.getX(), e.getY());
-        System.out.println("Pt drag =("+e.getX()+","+e.getY()+")");
-        Panel screen = (Panel) e.getSource();
-        screen.replaceLast(getNewShape(ptPress, ptDrag));
+        if (ptPress != null) {
+            replaceShape(e);
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        Point ptRel = new Point(e.getX(), e.getY());
-        //System.out.println(e.getX()+","+e.getY());
-        Panel screen = (Panel) e.getSource();
-        screen.replaceLast(getNewShape(ptPress, ptRel));
+        if (ptPress != null) {
+            replaceShape(e);
+            ptPress = null;
+        }
     }
 
-	public Shape getNewShape(Point ptPress, Point ptRel) {
+    private void replaceShape(MouseEvent e) {
+        Panel screen = (Panel) e.getSource();
+        double xMax = screen.getWidth() - 1;
+        double yMax = screen.getHeight() - 1;
+        double x = e.getX();
+        double y = e.getY();
+        x = x > xMax ? xMax : x < 0 ? 0 : x;
+        y = y > yMax ? yMax : y < 0 ? 0 : y;
+        screen.replaceLast(getNewShape(ptPress, new Point2D.Double(x, y)));
+    }
+
+    public Shape getNewShape(Point2D ptPress, Point2D ptRel) throws NoShapeChosenException {
         switch (shapeSelector) {
         case 1:
             return new Room(ptPress, ptRel);
@@ -61,5 +80,5 @@ public class Pinceau extends MouseAdapter {
         default:
             throw new NoShapeChosenException();
         }
-	}
+    }
 }
--- src/com/broteam/tipe/shape/Room.java
+++ src/com/broteam/tipe/shape/Room.java
@@ -1,28 +1,13 @@
 package com.broteam.tipe.shape;
 
-import java.awt.Graphics;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
-public class Room extends Shape {
+public class Room extends Rectangle2D.Double {
 
-	private Point topLeft;
-	private int width;
-	private int height;
-
-	Room(Point refPressed, Point refReleased) {
-		int topLeftX = Math.min(refPressed.x, refReleased.x);
-		int topLeftY = Math.min(refPressed.y, refReleased.y);
-
-		topLeft = new Point(topLeftX, topLeftY);
-
-		width = Math.max(refPressed.x, refReleased.x) - topLeftX;
-		height = Math.max(refPressed.y, refReleased.y) - topLeftY;
+	Room(Point2D refPressed, Point2D refReleased) {
+        super();
+        setFrameFromDiagonal(refPressed, refReleased);
 	}
 
-	@Override
-	public void draw(Graphics g) {
-		g.setColor(col);
-		int px = topLeft.x;
-		int py = topLeft.y;
-		g.drawRect(px, py, width, height);
-	}
 }
