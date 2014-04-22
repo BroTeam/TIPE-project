2c3cb0914b0d7b817cba6273a7daf8a4952f305e
Titouan BION <titouan.bion@gmail.com>
2014-04-22 19:45:57 +0200

    Color dans Material.java

--- src/com/broteam/tipe/testui/Material.java
+++ src/com/broteam/tipe/testui/Material.java
@@ -1,30 +1,37 @@
 package com.broteam.tipe.testui;
 
+import java.awt.Color;
+
 public enum Material {
 
-    THICK_GLASS("Vitre épaisse", 1.5),
-    WOOD("Bois", 2.5),
-    PLASTER("Plâtre", 4.0),
-    BOX("Box de bureau", 4.0),
-    GLASS_WALL("Cloison en verre", 7.0),
-    WATER("Eau", 7.0),
-    BRICK("Brique", 8.0),
-    CERAMIC("Céramique", 8.0),
-    ARMORED_GLASS("Verre blindé", 12.5),
-    CONCRETE("Béton", 12.5),
-    MIRROR("Mirroir", Double.POSITIVE_INFINITY),
-    METAL("Metal", Double.POSITIVE_INFINITY);
+    THICK_GLASS("Vitre épaisse", 1.5, Color.BLUE),
+    WOOD("Bois", 2.5, Color.GREEN),
+    PLASTER("Plâtre", 4.0, Color.RED),
+    BOX("Box de bureau", 4.0, Color.YELLOW),
+    GLASS_WALL("Cloison en verre", 7.0, Color.LIGHT_GRAY),
+    WATER("Eau", 7.0, Color.CYAN),
+    BRICK("Brique", 8.0, Color.ORANGE),
+    CERAMIC("Céramique", 8.0, Color.GRAY),
+    ARMORED_GLASS("Verre blindé", 12.5, Color.MAGENTA),
+    CONCRETE("Béton", 12.5, Color.BLACK),
+    MIRROR("Mirroir", Double.POSITIVE_INFINITY, Color.PINK),
+    METAL("Metal", Double.POSITIVE_INFINITY, Color.DARK_GRAY);
 
     private String displayName;
     private Double attenuation;
+    private Color colorMat;
 
-    private Material(String displayName, Double attenuation) {
+    private Material(String displayName, Double attenuation, Color colorMat) {
         this.displayName = displayName;
         this.attenuation = attenuation;
+        this.colorMat = colorMat;
     }
 
 	public Double getAttenuation() {
 		return attenuation;
+	}
+	public Color getColorMat() {
+		return colorMat;
 	}
 
     @Override
