948cccfbe7cfc98cc9315d6fa32b2b394713d474
Titouan BION <titouan.bion@gmail.com>
2014-05-01 12:57:04 +0200

    Ebauche mineure

--- src/com/broteam/tipe/element/Area.java
+++ src/com/broteam/tipe/element/Area.java
@@ -14,11 +14,20 @@ public class Area extends java.awt.geom.Area {
 	}
 	
 	Area(Wall wall, Point2D pa) {
-		Polygon p = new Polygon();
-		p.addPoint((int) ((Line2D) wall.getShape()).getP1().getX(), (int) ((Line2D) wall.getShape()).getP1().getY());
-		p.addPoint((int) ((Line2D) wall.getShape()).getP2().getX(), (int) ((Line2D) wall.getShape()).getP2().getY());
-		//On calcule ensuite le trapèze formé du mur avec le bord de la fenêtre
+		Polygon p = new Polygon(); 
+		p.addPoint((int) ((Line2D.Double) wall.getShape()).getP1().getX(), (int) ((Line2D.Double) wall.getShape()).getP1().getY());
+		p.addPoint((int) ((Line2D.Double) wall.getShape()).getP2().getX(), (int) ((Line2D.Double) wall.getShape()).getP2().getY());
+		//On calcule ensuite les points de la base du trapèze formé du mur avec le bord de la fenêtre
+		Point2D p1 = ((Line2D) wall.getShape()).getP1();
+		Line2D.Double line = new Line2D.Double(pa, p1);
+		Double coef1 = getCoefDirect(line);
+		// Equ droit1 : coef1*x + 
 		
+	}
+	
+	private static double getCoefDirect(Line2D l) {
+		// TODO test pour coef direct infini !
+		return ((l.getP2().getY() - l.getP1().getY())/(l.getP2().getX() - l.getP1().getX()));
 	}
 	
 }
