22dd244cf8165b76fbf770b6d123ba996e1beb9a
Joffrey Bion <joffrey.bion@gmail.com>
2014-05-12 01:24:12 +0200

    rewritten test using reflection for private method

--- test/com/broteam/tipe/math/ProjectionHelperTest.java
+++ test/com/broteam/tipe/math/ProjectionHelperTest.java
@@ -3,13 +3,18 @@ package com.broteam.tipe.math;
 import static org.junit.Assert.assertEquals;
 
 import java.awt.geom.Point2D;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.junit.Test;
 
 public class ProjectionHelperTest {
 
     @Test
-    public void testProjectionOnEdge() {
+    public void testProjectionOnEdge() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+        
+        Method getProjectionOnEdge = ProjectionHelper.class.getDeclaredMethod("getProjectionOnEdge", Point2D.class, Point2D.class, double.class, double.class);
+        getProjectionOnEdge.setAccessible(true);
 
         double width = 20;
         double height = 15;
@@ -21,32 +26,32 @@ public class ProjectionHelperTest {
         source.setLocation(3, 8);
         wayPoint.setLocation(5, 8);
         projection.setLocation(width, 8);
-        assertEquals(projection, ProjectionHelper.getProjectionOnEdge(source, wayPoint, width, height));
+        assertEquals(projection, getProjectionOnEdge.invoke(null, source, wayPoint, width, height));
 
         source.setLocation(5, 8);
         wayPoint.setLocation(3, 8);
         projection.setLocation(0, 8);
-        assertEquals(projection, ProjectionHelper.getProjectionOnEdge(source, wayPoint, width, height));
+        assertEquals(projection, getProjectionOnEdge.invoke(null, source, wayPoint, width, height));
 
         source.setLocation(5, 5);
         wayPoint.setLocation(5, 8);
         projection.setLocation(5, height);
-        assertEquals(projection, ProjectionHelper.getProjectionOnEdge(source, wayPoint, width, height));
+        assertEquals(projection, getProjectionOnEdge.invoke(null, source, wayPoint, width, height));
 
         source.setLocation(5, 8);
         wayPoint.setLocation(5, 3);
         projection.setLocation(5, 0);
-        assertEquals(projection, ProjectionHelper.getProjectionOnEdge(source, wayPoint, width, height));
+        assertEquals(projection, getProjectionOnEdge.invoke(null, source, wayPoint, width, height));
 
         source.setLocation(5, 5);
         wayPoint.setLocation(7, 7);
         projection.setLocation(Math.min(width, height), Math.min(width, height));
-        assertEquals(projection, ProjectionHelper.getProjectionOnEdge(source, wayPoint, width, height));
+        assertEquals(projection, getProjectionOnEdge.invoke(null, source, wayPoint, width, height));
 
         source.setLocation(7, 7);
         wayPoint.setLocation(5, 5);
         projection.setLocation(0, 0);
-        assertEquals(projection, ProjectionHelper.getProjectionOnEdge(source, wayPoint, width, height));
+        assertEquals(projection, getProjectionOnEdge.invoke(null, source, wayPoint, width, height));
     }
 
 }
