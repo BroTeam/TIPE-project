02374080e24491a9049b03ef6af5ae1c3fd83936
Joffrey BION <joffrey.bion@gmail.com>
2014-05-11 11:53:43 +0200

    added javadoc for Element

--- src/com/broteam/tipe/element/Element.java
+++ src/com/broteam/tipe/element/Element.java
@@ -2,8 +2,17 @@ package com.broteam.tipe.element;
 
 import java.awt.Graphics2D;
 
+/**
+ * Represents any element that can be drawn.
+ */
 public abstract class Element {
 
+    /**
+     * Draws this element using the specified {@link Graphics2D}.
+     * 
+     * @param g2d
+     *            The {@link Graphics2D} to use to draw.
+     */
     public abstract void drawSelf(Graphics2D g2d);
-    
+
 }
