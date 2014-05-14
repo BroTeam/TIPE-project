866046f0467b6b9ed90091e67eafde5839d6af68
Titouan BION <titouan.bion@gmail.com>
2014-05-14 17:44:14 +0200

    Ebauche séparation Panel/Model/Simulation

---
+++ src/com/broteam/tipe/model/Simulation.java
@@ -0,0 +1,30 @@
+package com.broteam.tipe.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.broteam.tipe.model.elements.AccessPoint;
+import com.broteam.tipe.model.elements.Obstacle;
+import com.broteam.tipe.signal.AreaIntersector;
+import com.broteam.tipe.signal.SignalArea;
+
+public class Simulation {
+
+    private List<SignalArea> areas = new LinkedList<>();
+    
+    public List<SignalArea> getSignalAreas() {
+    	return areas;
+    }
+
+    public void launchSimulation(AccessPoint ap, LinkedList<Obstacle> obstacles, double panelWidth, double panelHeight) {
+        LinkedList<SignalArea> overlappingAreas = new LinkedList<>();
+        for (Obstacle o : obstacles) {
+            if (o != null) {
+                overlappingAreas.addAll(o.getAttenuatedAreas(ap, panelWidth, panelHeight));
+            }
+        }
+        this.areas = AreaIntersector.createExclusiveAreas(overlappingAreas);
+        //repaint();
+    }
+    
+}
