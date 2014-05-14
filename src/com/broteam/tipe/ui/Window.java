a3d0cc2836d9f12ceb0478065a8c89c64e1bb079
Joffrey Bion <joffrey.bion@gmail.com>
2014-05-14 20:21:59 +0200

    removed unused import

--- src/com/broteam/tipe/ui/Window.java
+++ src/com/broteam/tipe/ui/Window.java
@@ -3,7 +3,6 @@ package com.broteam.tipe.ui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridLayout;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
