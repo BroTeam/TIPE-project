1ce8e44b5f99868352143c484da03b3d6f61f67a
Joffrey Bion <joffrey.bion@gmail.com>
2014-05-18 21:53:00 +0200

    extracted opacity constant

--- src/com/broteam/tipe/ui/DrawableArea.java
+++ src/com/broteam/tipe/ui/DrawableArea.java
@@ -9,10 +9,13 @@ import com.broteam.tipe.signal.Simulation;
 
 public class DrawableArea {
 
+    /** Opacity of the {@link DrawableArea}s. */
+    private static final int OPACITY = 180;
+
     private Simulation simulation;
     private SignalArea area;
     private BufferedImage image;
-    
+
     private int xPosition;
     private int yPosition;
 
@@ -21,7 +24,7 @@ public class DrawableArea {
         this.simulation = simulation;
         Rectangle bounds = area.getBounds();
         xPosition = bounds.x;
-        yPosition = bounds.y;        
+        yPosition = bounds.y;
         this.image = new BufferedImage(bounds.width, bounds.height, BufferedImage.TYPE_INT_ARGB);
         paintIntoImage();
     }
@@ -47,11 +50,11 @@ public class DrawableArea {
     private int getColor(int x, int y) {
         double power = simulation.getPower(x, y, area);
         int scaledPower = (int) (power * 255d / 100d);
-        int alpha = 180; // opaque
         int red = 255 - scaledPower;
         int green = scaledPower;
-        int argb = (alpha << 24) + (red << 16) + (green << 8);
-        System.out.println("power = " + power + "  R = " + red + "  G = " + green + "  argb = 0x" + Integer.toHexString(argb));
+        int argb = (OPACITY << 24) + (red << 16) + (green << 8);
+        System.out.println("power = " + power + "  R = " + red + "  G = " + green + "  argb = 0x"
+                + Integer.toHexString(argb));
         return argb;
     }
 
