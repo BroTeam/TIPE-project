994da6e6060bda0e3d0662a4f6e8502ebd307165
Joffrey Bion <joffrey.bion@gmail.com>
2014-05-19 00:46:31 +0200

    added debug boolean to toggle display of areas boundaries

--- src/com/broteam/tipe/ui/Panel.java
+++ src/com/broteam/tipe/ui/Panel.java
@@ -1,5 +1,6 @@
 package com.broteam.tipe.ui;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,6 +15,8 @@ import com.broteam.tipe.signal.SignalArea;
 import com.broteam.tipe.signal.Simulation;
 
 public class Panel extends JPanel {
+
+    private static final boolean DRAW_AREA_BOUNDARIES = false;
 
     private Window window;
     private List<DrawableArea> simulationAreas = new LinkedList<>();
@@ -39,19 +42,43 @@ public class Panel extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         Model m = window.getModel();
         if (m == null) {
-            g.setColor(Color.GRAY);
-            g.fillRect(0, 0, getWidth(), getHeight());
+            paintBackground(g2d, Color.GRAY);
             return; // nothing to draw
         }
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        paintBackground(g2d, Color.WHITE);
+
         // areas before elements, so that we can see the elements
+        paintAreas(g2d);
+        paintAreasBoundaries(g2d);
+
+        // draw elements on top of areas
+        paintElements(g2d, m);
+    }
+
+    private void paintAreas(Graphics2D g2d) {
         for (DrawableArea area : simulationAreas) {
             g2d.setClip(area.getClip());
             g2d.drawImage(area.getImage(), area.getX(), area.getY(), null);
         }
-        // draw elements on top of areas
         g2d.setClip(null);
+    }
+
+    private void paintAreasBoundaries(Graphics2D g2d) {
+        if (DRAW_AREA_BOUNDARIES) {
+            g2d.setColor(Color.GRAY);
+            final float dash1[] = { 4.0f };
+            final BasicStroke dashed = new BasicStroke(1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 5.0f,
+                    dash1, 0.0f);
+            g2d.setStroke(dashed);
+            // draw the boundaries of the areas
+            for (DrawableArea area : simulationAreas) {
+                g2d.draw(area.getClip());
+            }
+            g2d.setStroke(new BasicStroke());
+        }
+    }
+
+    private static void paintElements(Graphics2D g2d, Model m) {
         for (Element e : m.getElements()) {
             if (e != null) {
                 e.drawSelf(g2d);
@@ -59,4 +86,8 @@ public class Panel extends JPanel {
         }
     }
 
+    private void paintBackground(Graphics2D g2d, Color c) {
+        g2d.setColor(c);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+    }
 }
